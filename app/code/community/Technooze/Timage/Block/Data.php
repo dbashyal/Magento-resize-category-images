e847b3db5cbba0213e043056a2e47d1b2ae0976d
damu <damodar@acidgreen.com.au>
2013-07-03 11:20:05 +1000

    added author url

--- app/code/community/Technooze/Timage/Block/Data.php
+++ app/code/community/Technooze/Timage/Block/Data.php
@@ -2,7 +2,7 @@
 /**
 * @category   Technooze/Modules/magento-how-tos
 * @package    Technooze_Timage
-* @author     Damodar Bashyal
+* @author     Damodar Bashyal (http://dltr.org/)
 * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 */
 class Technooze_Timage_Block_Data extends Mage_Core_Block_Template
