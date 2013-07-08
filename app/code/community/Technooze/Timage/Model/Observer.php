a9f8ee5fc33fa3d4f92db62b7ce54323a5840b3c
damu <damodar@acidgreen.com.au>
2013-07-08 16:24:09 +1000

    added option to delete timage cache from cache management

---
+++ app/code/community/Technooze/Timage/Model/Observer.php
@@ -0,0 +1,57 @@
+<?php
+/**
+* @category   Technooze/Modules/magento-how-tos
+* @package    Technooze_Timage
+* @author     Damodar Bashyal (http://dltr.org/)
+* @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*/
+class Technooze_Timage_Model_Observer
+{
+    /**
+     * Is Enabled timage category image cache
+     *
+     * @var bool
+     */
+    protected $_isEnabled;
+
+    /**
+     * Class constructor
+     */
+    public function __construct()
+    {
+        $this->_isEnabled = Mage::app()->useCache('timage');
+    }
+
+    /**
+     * Check if full page cache is enabled
+     *
+     * @return bool
+     */
+    public function isCacheEnabled()
+    {
+        return $this->_isEnabled;
+    }
+
+    /**
+     * Clean full page cache
+     *
+     * @return Technooze_Timage_Model_Observer
+     */
+    public function cleanCache()
+    {
+        $cacheDir = Mage::getBaseDir('media') . DS . 'catalog' . DS . 'cache';
+        mageDelTree($cacheDir);
+        @mkdir($cacheDir);
+        return $this;
+    }
+
+    /**
+     * Invalidate timage cache @todo
+     * @return Technooze_Timage_Model_Observer
+     */
+    public function invalidateCache()
+    {
+        Mage::app()->getCacheInstance()->invalidateType('timage');
+        return $this;
+    }
+}
