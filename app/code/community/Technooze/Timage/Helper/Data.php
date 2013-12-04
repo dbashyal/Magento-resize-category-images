7692aab965eadcd682d56e2421bc360f61d2546c
Damodar Bashyal <dbashyal@gmail.com>
2013-12-04 12:05:06 +1100

    added link to original article

--- app/code/community/Technooze/Timage/Helper/Data.php
+++ app/code/community/Technooze/Timage/Helper/Data.php
@@ -3,6 +3,7 @@
 * @category   Technooze/Modules/magento-how-tos
 * @package    Technooze_Timage
 * @author     Damodar Bashyal (http://dltr.org/)
+* @link       http://j.mp/resizeImage
 * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * ref: /lib/Varien/Image.php
 */
